db95657977e1ad572577d6c73937f464c7c7407b
Danis <danshadance@mail.ru>
2022-11-28 23:01:44 +0300

    task4 Решена

---
+++ task4/task4.cs
@@ -0,0 +1,17 @@
+﻿// Выводим случайное трехзначное число
+// Выводим число без второй цифры
+
+int num = new Random().Next(100, 1000);
+
+int num1 = num / 100;
+
+int num2 = (num / 10) % 10;
+
+int num3 = num % 10;
+
+Console.WriteLine("Случайное трехзначное число: " + num);
+Console.Write ("Введеное число без второй цифры: ");
+Console.Write(num1);
+Console.Write(num3);
+
+// Решено
