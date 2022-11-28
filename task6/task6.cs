f3b370ded3b01fe6d3635a2e6c2d1607eaff2563
Danis <danshadance@mail.ru>
2022-11-29 00:08:24 +0300

    task6 Решена

---
+++ task6/task6.cs
@@ -0,0 +1,35 @@
+﻿// Принимаем на вход число
+// Кратно ли это число одновременно 7 и 23
+
+int num = 46;
+
+// int num = new Random().Next(100, 1000);
+
+Console.WriteLine("Является ли введеное число одновременно кратно 7 и 23?");
+Console.WriteLine("Сгенерированное число: " + num);
+
+int num2 = 7;
+
+int num3 = 23;
+
+int resnum2 = num / num2;
+
+if (resnum2 * num2 == num)
+{
+    int resnum3 = num / num3;
+    if (resnum3 * num3 == num)
+    {
+        Console.WriteLine("Ответ: Да");
+    }
+    else
+    {
+        Console.WriteLine("Ответ: Нет");
+    }
+}
+
+if (resnum2 * num2 != num)
+{
+    Console.WriteLine("Ответ: Нет");
+}
+
+
